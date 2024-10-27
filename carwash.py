8b1f06e88997466c5e5942775b496c315d521c00
DevEduardoReyes <jereyesb@unah.hn>
2024-10-26 21:52:10 -0600

    implementando funciones
    programa terminado falta hacer una abstraccion

--- carwash.py
+++ carwash.py
@@ -1,3 +1,4 @@
+
 class cliente:
     def __int__(self, identidad, nombre, apellido):
         self.identidad = identidad
@@ -17,3 +18,29 @@ class orden(cliente,auto):
         self.fecha_inicio = fecha_inicio
         self.fecha_fin = fecha_fin
 
+    def lavado_aspirado(self):
+        if self.tipo == 'turismo' or self.tipo == "coche":
+            precio = 200
+        else:
+            precio =500
+        return precio
+
+    def Shamposeado(self):
+        if self.asientos <= 2:
+            precio = 3000
+        else :
+            precio = 5000
+        return precio
+
+    def Expotador(self):
+        if self.year <= 2000:
+            precio = 1500
+        elif self.year >2000 and self.year < 2010:
+            precio =2000
+        else:
+            precio = 3500
+        return precio
+
+nuevo = orden(1999,"eduardo",'reyes','camioneta',2018,2,18,20)
+
+print(nuevo.lavado_aspirado())
